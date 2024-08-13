24aaf42ef93bf4cfbeaaa4b86befeda2c38a5dc7
Slets <guilhermevitor920@gmail.com>
2024-08-12 21:41:50 -0300

    input adicionado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        Scanner leia = new Scanner(System.in);
+
+        
+        System.out.println("Digite o seu nome: ");
+        nomeCliente = leia.nextLine();
+        
+        System.out.println("Digite sua agencia: ");
+        agencia = leia.nextLine();
+
+        System.out.println("Digite numero da conta: ");
+        numeroConta = leia.nextInt();
+
+        System.out.println("Digite seu saldo: ");
+        saldo = leia.nextDouble();
+
     }
 }
